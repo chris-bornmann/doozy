d2595969e2e73163a57e3c4796e0e56e6fc30bbe
Christopher Bornmann <chris.bornmann@pobox.com>
2026-03-28 13:29:50 -0700

    add swagger command

--- src/cli/main.py
+++ src/cli/main.py
@@ -1,11 +1,12 @@
 import typer
 
-from cli import db, items, users
+from cli import db, items, open_api, users
 
 app = typer.Typer()
 
 app.add_typer(db.app, name="db")
 app.add_typer(items.app, name="items")
+app.add_typer(open_api.cli_app, name="swagger")
 app.add_typer(users.app, name="users")
 
 
